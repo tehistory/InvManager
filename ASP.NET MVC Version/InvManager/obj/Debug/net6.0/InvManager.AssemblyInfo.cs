433fe0da8b10f2c19ab238fc422f21b162644cef
tehistory <ftegelhoff@gmail.com>
2022-04-13 08:47:28 -0600

    docker container created

--- ASP.NET MVC Version/InvManager/obj/Debug/net6.0/InvManager.AssemblyInfo.cs
+++ ASP.NET MVC Version/InvManager/obj/Debug/net6.0/InvManager.AssemblyInfo.cs
@@ -12,6 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: Microsoft.AspNetCore.Identity.UI.UIFrameworkAttribute("Bootstrap5")]
+[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("64250780-f856-4f39-80bb-ff10ae648f43")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("InvManager")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
